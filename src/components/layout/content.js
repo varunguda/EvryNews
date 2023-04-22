2ce1c7d09316dbcccb680c56469b421230f7127d
Varun Sai Guda <93998822+varunguda@users.noreply.github.com>
2023-04-22 16:03:26 +0530

    bug fix

--- src/components/layout/content.js
+++ src/components/layout/content.js
@@ -4,6 +4,7 @@ import './styles/navbar.css';
 
 import NavbarSecondary from '../elements/navbar2';
 import FirstSegment from './first-segment.js';
+import SecondSegment from './second-segment';
 
 export default function Content() {
 
@@ -11,6 +12,7 @@ export default function Content() {
     <div className='main-container'>
       <NavbarSecondary />
       <FirstSegment />
+      <SecondSegment />
     </div>
   )
 }
---
+++ src/components/layout/second-segment.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import './styles/second-segment.css';
+
+export default function SecondSegment() {
+  return (
+    <div className='second-segment-container'>
+      <div className='second-seg-head-container'>
+        I
+      </div>
+    </div>
+  )
+}
