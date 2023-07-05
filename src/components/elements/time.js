9d086097d6e4c9b68cb11a56a9d5dc0ca8ce524b
Varun Sai Guda <93998822+varunguda@users.noreply.github.com>
2023-07-05 18:04:45 +0530

    time bug fix

--- src/components/elements/time.js
+++ src/components/elements/time.js
@@ -9,31 +9,13 @@ export default function Time() {
             setCurrentDate(new Date().toLocaleString());
         }, 1000);
 
-
         return ()=> clearInterval(interval)
     }, []);
 
-    let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-    const getMonthName = (num) =>{
-        return months[num-1];
-    }
-
-    const getMonth = (str) =>{
-        let res = '';
-        for(let i=0; i<str.length; i++){
-            if(str[i]==='/'){
-                return res;
-            }
-            else{
-                res += str[i];
-            }
-        }
-        return getMonthName(Number.parseInt(res));
-    }
-
   return (
-    <div>
-        {getMonth(currentDate) + ' '+ currentDate.slice(0,2) + ', '+ currentDate.slice(5,9) + ' | '+ currentDate.slice(11).toUpperCase()}
+      <div>
+        {console.log(currentDate)}
+        {currentDate}
     </div>
   )
 }
