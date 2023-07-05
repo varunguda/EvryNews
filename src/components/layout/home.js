a3f1a4f186a13775571dce133d92c13b906e5d1e
Varun Sai Guda <93998822+varunguda@users.noreply.github.com>
2023-07-05 21:03:15 +0530

    refactor

--- src/components/layout/home.js
+++ src/components/layout/home.js
@@ -3,15 +3,15 @@ import React from 'react';
 import './styles/navbar.css';
 
 
-import FirstSegment from '../elements/element structures/first-segment.js';
-import SecondSegment from '../elements/element structures/second-segment';
-import ThirdSegment from '../elements/element structures/third-segment';
-import FourthSegment from '../elements/element structures/fourth-segment';
-import FifthSegment from '../elements/element structures/fifth-segment';
-import SixthSegment from '../elements/element structures/sixth-segment';
-import SeventhSegment from '../elements/element structures/seventh-segment';
-import EighthSegment from '../elements/element structures/eighth-segment';
-import FooterSegment from '../elements/element structures/footer';
+import FirstSegment from '../elements/element-structures/first-segment.js';
+import SecondSegment from '../elements/element-structures/second-segment';
+import ThirdSegment from '../elements/element-structures/third-segment';
+import FourthSegment from '../elements/element-structures/fourth-segment';
+import FifthSegment from '../elements/element-structures/fifth-segment';
+import SixthSegment from '../elements/element-structures/sixth-segment';
+import SeventhSegment from '../elements/element-structures/seventh-segment';
+import EighthSegment from '../elements/element-structures/eighth-segment';
+import FooterSegment from '../elements/element-structures/footer';
 
 export default function Home() {
 
